ec8853462dc40b1997f32dcc6aa327aa199165d8
arnii2511 <150372059+arnii2511@users.noreply.github.com>
2025-11-21 08:17:12 +0530

    Delete frontend directory

--- frontend/src/services/registrations.js
+++
@@ -1,9 +0,0 @@
-import API from './api';
-
-export async function registerForEvent(eventId) {
-  return API.post('/registrations/register', { eventId });
-}
-
-export async function getMyRegistrations() {
-  return API.get('/registrations/my');
-}
